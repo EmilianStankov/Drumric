a38e14a762b7b0d2324c55fdf26b07c4492a99fd
EmilianStankov <eminstine@abv.bg>
2015-02-01 15:48:01 +0200

    Add example layout

--- src/com/emilianstankov/drumric/MainActivity.java
+++ src/com/emilianstankov/drumric/MainActivity.java
@@ -1,9 +1,15 @@
 package com.emilianstankov.drumric;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +18,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button pad1 = (Button)findViewById(R.id.pad1);
+        Button pad2 = (Button)findViewById(R.id.pad2);
+        Button pad3 = (Button)findViewById(R.id.pad3);
+        Button pad4 = (Button)findViewById(R.id.pad4);
+        Button pad5 = (Button)findViewById(R.id.pad5);
+        Button pad6 = (Button)findViewById(R.id.pad6);
+        Button pad7 = (Button)findViewById(R.id.pad7);
+        Button pad8 = (Button)findViewById(R.id.pad8);
+        ArrayList<Button> pads = new ArrayList<Button>(Arrays.asList(pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8));
+        for(Button btn: pads){
+	        btn.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(MainActivity.this, "clicked", 100).show();
+				}
+			});
+        }
     }
 
 
